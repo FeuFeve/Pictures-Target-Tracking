456afe84a47e0fcbc69a3d7544fc29bd39740a16
FeuFeve <clement-potin@outlook.com>
2021-11-13 04:16:11 +0100

    Can now draw rectangles on image

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ import os
 targetPath = "targets/"
 resultsPath = "results/"
 
+img = None
+baseImage = None
+
+drag = False
+p1, p2 = [], []
+
 
 # ------------------------------------------------------------
 # Functions
@@ -27,12 +33,44 @@ def getTargetPicturesPaths():
     return files
 
 
+def mouseEvent(event, x, y, flags, params):
+    global drag, p1, p2
+    redraw = False
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        p1 = [x, y]
+        p2 = [x, y]
+        drag = True
+        redraw = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        p2 = [x, y]
+        drag = False
+        redraw = True
+        print("Selected pixels: " + str(tuple(p1)) + " to " + str(tuple(p2)))
+    
+    if drag:
+        p2 = [x, y]
+        redraw = True
+        
+    if redraw:
+        img = baseImage.copy()
+        img = cv2.rectangle(img, tuple(p1), tuple(p2), (0, 0, 255), 1)
+        cv2.imshow("First image", img)
+
+
 def openFirstFile():
+    global img, baseImage
+
     files = getTargetPicturesPaths()
 
     img = cv2.imread(files[0], cv2.IMREAD_COLOR)
+    baseImage = img.copy()
     cv2.imshow("First image", img)
 
+    cv2.setMouseCallback("First image", mouseEvent)
+
+
+def waitForExit():
     # Wait for a key to be pressed to exit
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -45,6 +83,7 @@ def openFirstFile():
 def main():
     print("\n\n\n## BEGIN ##")
     openFirstFile()
+    waitForExit()
     print("## END ##")
 
 
